28daca47dd3a6e003d56d52dd6c59d7a6227ed45
Amirkhon-Buriev <amirkhonburiyev@gmail.com>
2024-08-15 10:51:58 +0500

    Add files via upload

---
+++ JavaSkript.js
@@ -0,0 +1,19 @@
+let son = +prompt("1-son kiriting")
+let son2 = +prompt("2-son kiriting")
+
+let amal = prompt(`bajarish kerak bolgan amali talang
+1) +qoshish
+2) -airesh
+3) *kopaetirish
+4) /bolish
+`);
+
+console.log(son)
+console.log(son1)
+console.log(amal)
+
+if (amal = 1){
+    alert(`${son} + ${son2} = ${son + son2}`);
+}else if (alert =1) {
+    alert(`${son} ni ${son2}ga bolsa jovob: =${ son / son2}`);
+}
